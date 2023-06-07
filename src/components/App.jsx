f2b7cc6a8ae42783b5d95d81f96481aaef8a32a8
Lavi Reichman <lavireichman@gmail.com>
2023-06-07 15:45:59 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Layout from './Layout'
+import { Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
     <>
-     
+    <Routes>
+    <Route path='/' element={<Layout />}>
+      <Route element={<WhoAreYou/>}></Route>
+      <Route ></Route>
+    </Route>
+     </Routes>
     </>
   )
 }
